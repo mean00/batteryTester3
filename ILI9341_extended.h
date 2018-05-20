b1bc6d033ecb40f2bd7f22f98168d75abeb4ad33
mean <fixounet@free.fr>
2018-05-20 09:43:49 +0200

    [bat2] extended font management

--- ILI9341_extended.h
+++ ILI9341_extended.h
@@ -10,5 +10,9 @@ class ILI9314 : public Adafruit_ILI9341_STM
   public:
         ILI9314(int8_t _CS, int8_t _DC, int8_t _RST = -1);
         ~ILI9314();
-                
+    
+        size_t write(uint8_t c);
+        // Draw a character
+        void myDrawChar(int16_t x, int16_t y, unsigned char c,  uint16_t color, uint16_t bg, uint8_t size) ;
+        
 };
