0e694cb540063a1422133f76ce19fb18ee2bd7ff
mean <fixounet@free.fr>
2020-10-04 08:47:54 +0200

    switch to TFT_eSPI

--- backgrounds/fonts.cpp
+++ backgrounds/fonts.cpp
@@ -8,7 +8,7 @@ extern const GFXfont FreeSans24pt7b ;
 extern const GFXfont FreeSans18pt7b ;
 extern const GFXfont FreeSans9pt7b ;
 
-#include "extendedIli9341/Fonts/FreeSans24pt7b.h"
-#include "extendedIli9341/Fonts/FreeSans18pt7b.h"
-#include "extendedIli9341/Fonts/FreeSans9pt7b.h"
+#include "assets/fonts//FreeSans24pt7b.h"
+#include "assets/fonts//FreeSans18pt7b.h"
+#include "assets/fonts//FreeSans9pt7b.h"
 
--- screenInclude.h
+++ screenInclude.h
@@ -1,6 +1,4 @@
 #pragma once
 #include <Wire.h>
 #include "SPI.h"
-#include "Adafruit_GFX.h"
-#include "Adafruit_ILI9341_STM.h"
 #include "screenBase.h"
