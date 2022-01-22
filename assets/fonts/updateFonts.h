c3f16794fce7b0ef7ba49b384dd9015fc5a25c1f
mean <fixounet@free.fr>
2022-01-22 15:08:50 +0100

    cleanup fonts

---
+++ assets/fonts/updateFonts.h
@@ -0,0 +1,8 @@
+flatconvert      -f ttf/Arcane\ Nine.otf -s 32 -b 46 -e 57 -o 1bit.h -c
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o FreeSans18pt7b.h  -c -m freesane18_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o FreeSans18pt7b.h   -m freesane18_nc.bin
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o FreeSans9pt7b.h  -c -m freesan9_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o FreeSans9pt7b.h   -m freesan9_nc.bin
+flatconvert      -f ttf/FreeSans.ttf -s 32  -o FreeSans32pt7b.h  -c -m freesane32_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 32  -o FreeSans32pt7b.h   -m freesane32_nc.bin
+
