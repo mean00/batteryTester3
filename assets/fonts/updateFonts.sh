e2037803e0f1acfa7069b784381818e48d7b53c3
mean <fixounet@free.fr>
2022-01-27 07:16:12 +0100

    sync

---
+++ assets/fonts/updateFonts.sh
@@ -0,0 +1,42 @@
+flatconvert      -f ttf/Arcane\ Nine.otf -s 32 -b 46 -e 57 -p 2   -o 1bit.h  -c
+flatconvert      -f ttf/Arcane\ Nine.otf -s 32  -p 2   -o arcane2.h  -c
+flatconvert      -f ttf/Arcane\ Nine.otf -s 32  -p 1   -o arcane1.h  -c
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o FreeSans9pt7b_1bb.h   -p 1
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o FreeSans9pt7b_1bbc.h   -p 1 -c
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o FreeSans9pt7b.h   -p 2
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o FreeSans9pt7b_c.h   -p 2 -c
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o FreeSans18pt7b_1bpp.h   -p 1 -c
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o FreeSans18pt7b.h   -p 2 -c
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o FreeSans18pt7b_c.h   -p 2 -c
+#flatconvert      -f ttf/FreeSans.ttf -s 9  -o FreeSans9pt7b.h   -o smallnc.h 
+exit
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o FreeSans18pt7b.h  -c -m freesane18_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o FreeSans18pt7b.h   -m freesane18_nc.bin
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o FreeSans9pt7b.h  -c -m freesan9_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o FreeSans9pt7b.h   -m freesan9_nc.bin
+flatconvert      -f ttf/FreeSans.ttf -s 32  -o FreeSans32pt7b.h  -c -m freesane32_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 32  -o FreeSans32pt7b.h   -m freesane32_nc.bin
+flatconvert      -f ttf/FreeSans.ttf -s 32  -o FreeSans32pt7b_2b.h -p 2 -c -m freesane32_2b_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 32  -o FreeSans32pt7b_2b.h -p 2  -m freesane32_2b_nc.bin
+flatconvert      -f ttf/FreeSans.ttf -s 32  -o FreeSans32pt7b_2b.h -p 4 -c -m freesane32_4b_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 32  -o FreeSans32pt7b_2b.h -p 4  -m freesane32_4b_nc.bin
+flatconvert      -f ttf/FreeSans.ttf -s 32  -o FreeSans32pt7b_8b.h -p 8 -c -m freesane32_8b_c.bin
+
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o xFreeSans32pt7b.h  -c -m freesane9_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o xFreeSans32pt7b.h   -m freesane9_nc.bin
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o xFreeSans32pt7b_2b.h -p 2 -c -m freesane9_2b_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o xFreeSans32pt7b_2b.h -p 2  -m freesane9_2b_nc.bin
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o xFreeSans32pt7b_2b.h -p 4 -c -m freesane9_4b_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 9  -o xFreeSans32pt7b_2b.h -p 4  -m freesane9_4b_nc.bin
+
+
+
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o xFreeSans18pt7b.h  -c -m freesane19_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o xFreeSans18pt7b.h   -m freesane19_nc.bin
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o xFreeSans18pt7b_2b.h -p 2 -c -m freesane19_2b_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o xFreeSans18pt7b_2b.h -p 2  -m freesane19_2b_nc.bin
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o xFreeSans18pt7b_2b.h -p 4 -c -m freesane19_4b_c.bin
+flatconvert      -f ttf/FreeSans.ttf -s 18  -o xFreeSans18pt7b_2b.h -p 4  -m freesane19_4b_nc.bin
+
+
+
